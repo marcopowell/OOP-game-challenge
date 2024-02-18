7fb6fa485a160c8848c41fa6a0d4609b515f99ac
Marco Powell <mvapowell@gmail.com>
2024-02-17 21:48:58 -0600

    Game challenge is completed

--- script.js
+++ script.js
@@ -12,11 +12,29 @@ Player.prototype.gainXp = function (xp) {
         this.lvl++;
         this.points -= 10;
     }
-}
+
+    console.log(this.describe());
+};
+
 
 Player.prototype.describe = function () {
     return `${this.name} is level ${this.lvl} with ${this.points} XP`;
 }
 
+const player1 = new Player ('Marco');
+const player2 = new Player ('Antonio');
 
+player1.gainXp(8);
+player2.gainXp(3);
+player1.gainXp(9);
+player2.gainXp(2);
+player1.gainXp(6);
+player2.gainXp(4);
+player1.gainXp(6);
+player2.gainXp(3);
 
+console.log(player1);
+console.log(player2);
+
+console.log(player1.describe());
+console.log(player2.describe());
