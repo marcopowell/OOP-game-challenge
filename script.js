ed6d261743d98b5219c86cc396df728d12d22df2
Marco Powell <mvapowell@gmail.com>
2024-02-17 21:36:48 -0600

    Created construction function and added methods to the prototypeof the game

---
+++ script.js
@@ -0,0 +1,22 @@
+function Player (name) {
+    this.name = name;
+    this.lvl = 1;
+    this.points = 0;
+}
+
+//Adding a method to the prototype 
+Player.prototype.gainXp = function (xp) {
+    this.points += xp;
+
+    if (this.points >= 10){
+        this.lvl++;
+        this.points -= 10;
+    }
+}
+
+Player.prototype.describe = function () {
+    return `${this.name} is level ${this.lvl} with ${this.points} XP`;
+}
+
+
+
